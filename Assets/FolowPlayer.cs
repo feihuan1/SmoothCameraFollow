14190248660fb19e4a527a47029fa6bce4eb5889
feihuan1 <ericrealtor1997@gmail.com>
2024-07-24 23:10:19 -0700

    camera smooth

--- Assets/FolowPlayer.cs
+++ Assets/FolowPlayer.cs
@@ -3,11 +3,24 @@ using UnityEngine;
 public class FolowPlayer : MonoBehaviour
 {
 
-    public Transform Player;
-    Vector3 offset = new Vector3(0f, 0f, -20f);
+    public Transform player;
+    public Vector3 offset; 
 
-    void Update()
+    // the bigger , the faster the camera will lock on player(between 0-1)
+    public float smoothSpeed = 0.125f;
+
+// Update runs at same time, player and camera will completing each other
+    // void Update()
+    // {
+    //     transform.position = Player.position + offset;
+    // }
+    void FixedUpdate()
     {
-        transform.position = Player.position + offset;
+        
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position ,desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(player);
     }
 }
